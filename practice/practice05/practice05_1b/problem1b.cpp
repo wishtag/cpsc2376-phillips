94341298be0cb61868640d0b99e44f39c2e68ce2
HaydenPhillips777 <hlphillips1@ualr.edu>
2025-02-20 13:28:52 -0600

    Finished problem1b of Practice05

---
+++ practice/practice05/practice05_1b/problem1b.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+int main() {
+    std::vector<int> nums;
+    int int_count;
+    int input;
+
+    std::cout << "Enter the number of integers: ";
+    std::cin >> int_count;
+
+    std::cout << "Enter " << int_count << " integers: ";
+    for (int i = 0; i < int_count; ++i)
+    {
+        std::cin >> input;
+        nums.push_back(input);
+    }
+
+    std::cout << "The integers in reverse order are: ";
+    for (auto it = nums.rbegin(); it != nums.rend(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+
+    std::cout << "\n";
+
+    return 0;
+}
